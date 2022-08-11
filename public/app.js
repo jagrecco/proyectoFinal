c40de006d87b427a2c41a7a723e8f240e518eb88
jag <jagrecco@gmail.com>
2022-08-10 23:35:38 -0300

    Final entrega

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 document.getElementById("productos").addEventListener("click", ()=>{
     fetch('http://localhost:3000/api/productos')
-    // Exito
+    
     .then(response =>{
-        response.json()})  // convertir a json
+        response.json()})
     .then(
         json => {
             document.getElementById("lista").innerHTML=json
             console.log(json)
         }
-    )    //imprimir los datos en la consola
-    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+    )
+    .catch(err => console.log('Solicitud fallida', err)); 
 })
